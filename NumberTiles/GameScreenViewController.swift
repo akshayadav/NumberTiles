ae14569266a24eaa76ad75864b11d6b0fdede4ed
akshay yadav <akshay-yadav@uiowa.edu>
2016-07-22 01:31:22 -0600

    initial commit

---
+++ NumberTiles/GameScreenViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GameScreenViewController.swift
+//  NumberTiles
+//
+//  Created by akshay on 7/22/16.
+//  Copyright © 2016 akshay. All rights reserved.
+//
+
+import UIKit
+
+class GameScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        var imageView: UIImageView = UIImageView()
+//       imageView = UIImageView.init()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
