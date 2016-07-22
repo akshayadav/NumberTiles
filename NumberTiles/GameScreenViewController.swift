7b736be1639c3cf06efb580498f72230dc2448f3
akshay yadav <akshay-yadav@uiowa.edu>
2016-07-22 10:24:03 -0600

    setup Function working

--- NumberTiles/GameScreenViewController.swift
+++ NumberTiles/GameScreenViewController.swift
@@ -13,12 +13,56 @@ class GameScreenViewController: UIViewController {
     @IBOutlet weak var topBar: UIView!
     @IBOutlet weak var bottomBar: UIView!
     @IBOutlet weak var middleGround: UIView!
+    
+    var maxNumberInGame:Int = 12
+    var minNumberInGame: Int = 1
+    
+    //---------Buttons------------
+    
+    
+    @IBOutlet weak var C1R1: UIButton!
+    @IBOutlet weak var C2R1: UIButton!
+    @IBOutlet weak var C3R1: UIButton!
+    @IBOutlet weak var C1R2: UIButton!
+    @IBOutlet weak var C2R2: UIButton!
+    @IBOutlet weak var C3R2: UIButton!
+    @IBOutlet weak var C1R3: UIButton!
+    @IBOutlet weak var C2R3: UIButton!
+    @IBOutlet weak var C3R3: UIButton!
+    @IBOutlet weak var C1R4: UIButton!
+    @IBOutlet weak var C2R4: UIButton!
+    @IBOutlet weak var C3R4: UIButton!
+    
+    var arrayOfButtons: NSArray = NSArray()
+    
+    //----------------------------
+    
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+    
+        if(sender.currentTitle!.isEqual(String(minNumberInGame))){
+            minNumberInGame = minNumberInGame + 1
+            maxNumberInGame = maxNumberInGame + 1
+            setNumberOnButtons()
+        }
+        else{
+            print("wrong Number")
+        }
+        
+        //print(sender.currentTitle!)
+        
+    
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(topBar.frame.height)
         print(bottomBar.frame.height)
-
+        
+        arrayOfButtons = [C1R1, C2R1, C3R1, C1R2, C2R2, C3R2, C1R3, C2R3, C3R3, C1R4, C2R4, C3R4]
         // Do any additional setup after loading the view.
     }
 
@@ -35,13 +79,27 @@ class GameScreenViewController: UIViewController {
         super.viewDidAppear(animated)
     }
     
-    @IBAction func whatheight(sender: UIButton) {
+    func whatheight() {
         
         print(topBar.frame.height)
         print(bottomBar.frame.height)
         print(middleGround.frame.height)
         print(UIScreen.mainScreen().bounds.size.height)
     }
+    
+    func setNumberOnButtons(){
+    
+        var numbersInGame = [Int](minNumberInGame...maxNumberInGame)
+        
+        for button in arrayOfButtons {
+            let randomIndex = Int(arc4random_uniform(UInt32(numbersInGame.count)))
+            button.setTitle(String(numbersInGame[randomIndex]), forState: UIControlState.Normal)
+            numbersInGame.removeAtIndex(randomIndex)
+        }
+    
+    }
+    
+    
 
     /*
     // MARK: - Navigation
